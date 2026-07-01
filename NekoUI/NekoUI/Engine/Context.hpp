302534efd56b40d094c126e12ba04ff8608736cc
SUIMO <1992724048@qq.com>
2026-07-02 07:47:26 +0800

    feat: Widget 焦点系统 + Context::request_focus 回调

--- NekoUI/NekoUI/Engine/Context.hpp
+++ NekoUI/NekoUI/Engine/Context.hpp
@@ -6,12 +6,17 @@
 #include "Device/Keyboard.hpp"
 #include "Device/Mouse.hpp"
 
+namespace neko::widget {
+    class Widget;
+} // namespace neko::widget
+
 namespace neko::engine {
     struct Context {
         std::function<void()> rebuild;
         std::function<void()> rerender;
         std::function<void()> animation_start;
         std::function<void()> animation_end;
+        std::function<void(widget::Widget*)> request_focus;
 
         std::atomic_bool dirty;
         float dpi_scale = 1.0F;
--- NekoUI/NekoUI/Widget/Widget.hpp
+++ NekoUI/NekoUI/Widget/Widget.hpp
@@ -9,7 +9,6 @@
 #include "../Backend/Backend.hpp"
 
 namespace neko::widget {
-
     struct Constraints {
         int x = 0, y = 0;
         int width = INT_MAX;
@@ -17,45 +16,95 @@ namespace neko::widget {
     };
 
     class Widget {
-        template<typename T> friend class Sub;
-
+        template<typename T>
+        friend class Sub;
+        friend class neko::engine::Engine;
     public:
         virtual ~Widget() = default;
 
         virtual auto draw(engine::Context& context, backend::Backend& backend) -> void;
         virtual auto layout(Constraints constraints) -> void;
         virtual auto update(engine::Context& context) -> void;
-        virtual auto handle_event(engine::Context& context, UINT msg,
-                                  WPARAM wparam, LPARAM lparam) -> bool;
+        virtual auto handle_event(engine::Context& context, UINT msg, WPARAM wparam, LPARAM lparam) -> bool;
 
-        [[nodiscard]] virtual auto hit_test(const neko::mouse::Mouse& mouse) const -> bool;
+        [[nodiscard]] virtual auto hit_test(const mouse::Mouse& mouse) const -> bool;
 
         auto mark_dirty() -> void;
-        [[nodiscard]] auto dirty() const -> bool { return m_dirty; }
-        auto clear_dirty() -> void { m_dirty = false; }
 
-        [[nodiscard]] auto children() const -> const std::vector<Widget*>& { return m_children; }
-        [[nodiscard]] auto parent() const -> Widget* { return m_parent; }
-        [[nodiscard]] auto child_count() const -> size_t { return m_children.size(); }
+        [[nodiscard]] auto dirty() const -> bool {
+            return m_dirty;
+        }
+
+        auto clear_dirty() -> void {
+            m_dirty = false;
+        }
+
+        [[nodiscard]] auto children() const -> const std::vector<Widget*>& {
+            return m_children;
+        }
+
+        [[nodiscard]] auto parent() const -> Widget* {
+            return m_parent;
+        }
+
+        [[nodiscard]] auto child_count() const -> size_t {
+            return m_children.size();
+        }
+
         [[nodiscard]] auto root() -> Widget*;
 
         auto set_bounds(int x, int y, int w, int h) -> void;
         auto set_bounds(glm::ivec4 bounds) -> void;
-        [[nodiscard]] auto bounds() const -> const glm::ivec4& { return m_bounds; }
-        [[nodiscard]] auto x() const -> int { return m_bounds.x; }
-        [[nodiscard]] auto y() const -> int { return m_bounds.y; }
-        [[nodiscard]] auto width() const -> int { return m_bounds.z; }
-        [[nodiscard]] auto height() const -> int { return m_bounds.w; }
 
-        auto set_z_order(int order) -> void { m_z_order = order; }
-        [[nodiscard]] auto z_order() const -> int { return m_z_order; }
+        [[nodiscard]] auto bounds() const -> const glm::ivec4& {
+            return m_bounds;
+        }
+
+        [[nodiscard]] auto x() const -> int {
+            return m_bounds.x;
+        }
+
+        [[nodiscard]] auto y() const -> int {
+            return m_bounds.y;
+        }
+
+        [[nodiscard]] auto width() const -> int {
+            return m_bounds.z;
+        }
+
+        [[nodiscard]] auto height() const -> int {
+            return m_bounds.w;
+        }
+
+        auto set_z_order(const int order) -> void {
+            m_z_order = order;
+        }
+
+        [[nodiscard]] auto z_order() const -> int {
+            return m_z_order;
+        }
 
         auto set_visible(bool v) -> void;
-        [[nodiscard]] auto visible() const -> bool { return m_visible; }
 
+        [[nodiscard]] auto visible() const -> bool {
+            return m_visible;
+        }
+
+        [[nodiscard]] virtual auto focusable() const -> bool {
+            return false;
+        }
+
+        virtual auto on_focus_gained() -> void {}
+
+        virtual auto on_focus_lost() -> void {}
+
+        [[nodiscard]] auto has_focus() const -> bool {
+            return m_has_focus;
+        }
     protected:
         Widget() = default;
 
+        bool m_has_focus = false;
     private:
         auto register_child(Widget* child) -> void;
         auto unregister_child(Widget* child) -> void;
@@ -69,8 +118,6 @@ namespace neko::widget {
         bool m_dirty = true;
         bool m_visible = true;
     };
-
-    // === inline implementations ===
 
     inline auto Widget::draw(engine::Context& context, backend::Backend& backend) -> void {
         for (auto* child : children_sorted_asc()) {
@@ -87,17 +134,24 @@ namespace neko::widget {
         }
     }
 
-    inline auto Widget::handle_event(engine::Context& context, UINT msg,
-                                     WPARAM wparam, LPARAM lparam) -> bool
-    {
+    inline auto Widget::handle_event(engine::Context& context, const UINT msg, const WPARAM wparam, const LPARAM lparam) -> bool {
         for (auto* child : children_sorted_desc()) {
             if (child->handle_event(context, msg, wparam, lparam)) {
                 return true;
             }
         }
         switch (msg) {
-            case WM_MOUSEMOVE:
             case WM_LBUTTONDOWN:
+                if (hit_test(context.mouse)) {
+                    if (focusable()) {
+                        if (context.request_focus) {
+                            context.request_focus(this);
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            case WM_MOUSEMOVE:
             case WM_LBUTTONUP:
             case WM_RBUTTONDOWN:
             case WM_RBUTTONUP:
@@ -113,21 +167,23 @@ namespace neko::widget {
         }
     }
 
-    inline auto Widget::hit_test(const neko::mouse::Mouse& mouse) const -> bool {
-        if (!m_visible) return false;
+    inline auto Widget::hit_test(const mouse::Mouse& mouse) const -> bool {
+        if (!m_visible) {
+            return false;
+        }
         return mouse.is_inside(m_bounds);
     }
 
     inline auto Widget::mark_dirty() -> void {
         m_dirty = true;
-        if (m_parent) {
+        if (m_parent != nullptr) {
             m_parent->mark_dirty();
         }
     }
 
     inline auto Widget::root() -> Widget* {
         auto* cur = this;
-        while (cur->m_parent) {
+        while (cur->m_parent != nullptr) {
             cur = cur->m_parent;
         }
         return cur;
@@ -138,12 +194,12 @@ namespace neko::widget {
         mark_dirty();
     }
 
-    inline auto Widget::set_bounds(glm::ivec4 bounds) -> void {
+    inline auto Widget::set_bounds(const glm::ivec4 bounds) -> void {
         m_bounds = bounds;
         mark_dirty();
     }
 
-    inline auto Widget::set_visible(bool v) -> void {
+    inline auto Widget::set_visible(const bool v) -> void {
         m_visible = v;
         mark_dirty();
     }
@@ -174,5 +230,4 @@ namespace neko::widget {
         std::ranges::sort(sorted, std::ranges::greater{}, &Widget::z_order);
         return sorted;
     }
-
 } // namespace neko::widget
