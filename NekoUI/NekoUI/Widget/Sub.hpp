54201e3d821b641816694a14312037f6a519e3ae
SUIMO <1992724048@qq.com>
2026-07-02 07:17:43 +0800

    feat: 添加 Sub<T> 自动注册包装器

---
+++ NekoUI/NekoUI/Widget/Sub.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Widget.hpp"
+
+namespace neko::widget {
+
+    template<typename T>
+    class Sub : public T {
+        static_assert(std::is_base_of_v<Widget, T>);
+
+    public:
+        template<typename... Args>
+        explicit Sub(Widget* parent, Args&&... args)
+            : T(std::forward<Args>(args)...)
+        {
+            if (parent) {
+                Widget::register_child(this);
+            }
+        }
+
+        ~Sub() noexcept {
+            if (this->m_parent) {
+                Widget::unregister_child(this);
+            }
+        }
+
+        Sub(const Sub&) = delete;
+        auto operator=(const Sub&) -> Sub& = delete;
+        Sub(Sub&&) = default;
+        auto operator=(Sub&&) -> Sub& = default;
+    };
+
+} // namespace neko::widget
