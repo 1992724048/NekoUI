2a8d9feb5a77342f2c0852148aa06be2276058ba
遂沫 <1992724048@qq.com>
2026-08-10 09:07:24 +0800

    style(behavior): GeometryState 精简注释 + BOM（用户清理）

--- NekoUI/NekoUI/Behavior/GeometryState.hpp
+++ NekoUI/NekoUI/Behavior/GeometryState.hpp
@@ -1,4 +1,4 @@
-// 2026-08-10
+﻿// 2026-08-10
 
 #pragma once
 
@@ -7,7 +7,6 @@
 namespace neko::behavior {
     using namespace neko::type;
 
-    // 行为间共享的几何状态（布局写、绘制/命中/输入读；生命周期 = 持有控件）
     struct GeometryState {
         Vec4I bounds{};
     };
